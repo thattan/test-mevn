9e11e18b8ced0b44374446f2492a06d2f2d97d0e
Tyler Hattan <tylerhattan@yahoo.com>
2023-04-18 00:02:37 -0500

    second commit

--- client/src/router.js
+++ client/src/router.js
@@ -17,6 +17,11 @@ export default new Router({
       path: "/about",
       name: "about",
       component: () => import("./views/About.vue")
+    },
+    {
+      path: "/new-page",
+      name: "newpage",
+      component: () => import("./views/NewPage.vue")
     }
   ]
 });
---
+++ client/src/views/NewPage.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="new-page">
+        <h1>This is a new page</h1>
+    </div>
+</template>
+  
